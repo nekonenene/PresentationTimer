b56547d7118946a62bb7de531a126442f70c56ad
ハトネコエ (nekonenene) <hato.nekoe@gmail.com>
2017-07-10 18:40:49 +0900

    listenerを持つテキストの作成

--- app/src/main/java/com/x0/hatonekoe/presentation_timer/MainActivityFragment.kt
+++ app/src/main/java/com/x0/hatonekoe/presentation_timer/MainActivityFragment.kt
@@ -1,10 +1,13 @@
 package com.x0.hatonekoe.presentation_timer
 
-import android.support.v4.app.Fragment
 import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+
+import kotlinx.android.synthetic.main.fragment_main.*
 
 /**
  * A placeholder fragment containing a simple view.
@@ -14,5 +17,15 @@ class MainActivityFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_main, container, false)
+    }
+
+    override fun onStart() {
+        super.onStart()
+
+        Log.d(this.javaClass.name, "Hello World!!")
+
+        button.setOnClickListener {
+            Log.d(this.javaClass.name, "押されたよ")
+        }
     }
 }
