6979efa82bc72e3790cb88a0f0a6855ab4e51038
ハトネコエ (nekonenene) <hato.nekoe@gmail.com>
2017-07-16 04:17:18 +0900

    アラーム音を鳴らす仕組みに

--- app/src/main/java/com/x0/hatonekoe/presentation_timer/service/util/PresentationCountDownTimer.kt
+++ app/src/main/java/com/x0/hatonekoe/presentation_timer/service/util/PresentationCountDownTimer.kt
@@ -1,8 +1,11 @@
 package com.x0.hatonekoe.presentation_timer.service.util
 
+import android.content.Context
+import android.media.MediaPlayer
 import android.os.CountDownTimer
 import android.util.Log
 import com.x0.hatonekoe.presentation_timer.PresentationTimerApp
+import com.x0.hatonekoe.presentation_timer.R
 import com.x0.hatonekoe.presentation_timer.model.entity.TimerStatus
 import com.x0.hatonekoe.presentation_timer.presentation.presenter.MainFragmentPresenterIF
 import com.x0.hatonekoe.presentation_timer.service.usecase.CurrentTimeUseCaseIF
@@ -11,12 +14,16 @@ import javax.inject.Inject
 class PresentationCountDownTimer(millisInFuture: Long, countDownInterval: Long): CountDownTimer(millisInFuture, countDownInterval) {
 
     @Inject
+    lateinit var mContext: Context
+    @Inject
     lateinit var mUseCase: CurrentTimeUseCaseIF
     @Inject
     lateinit var mPresenter: MainFragmentPresenterIF
+    var mPlayer: MediaPlayer
 
     init {
         injectSelf()
+        mPlayer = MediaPlayer.create(mContext, R.raw.alarm)
     }
 
     // init内はコンパイルされないようなので、injectは外側で関数化
@@ -31,6 +38,8 @@ class PresentationCountDownTimer(millisInFuture: Long, countDownInterval: Long):
 
     override fun onFinish() {
         Log.d(this.javaClass.name, "時間終了です！！！")
+        mPlayer.seekTo(0)
+        mPlayer.start()
         mUseCase.setTimerStatus(TimerStatus.FINISH)
     }
 }
